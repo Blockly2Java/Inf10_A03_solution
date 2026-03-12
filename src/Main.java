8e0a7dce5a66aa832e57badb8e1422ce520a6e01
Valentin Herrmann <accounts@valentin-herrmann.com>
2026-03-12 23:16:45 +0100

    template

--- src/Main.java
+++ src/Main.java
@@ -15,10 +15,31 @@ public class Main {
      */
     public static void main() {
         System.out.println(addieren(1, 2));
+        System.out.println(subtrahieren(1, 2));
+        System.out.println(multiplizieren(1, 2));
+        System.out.println(dividieren(1, 2));
+        System.out.println(mittelwert(1, 2));
     }
 
     public static double addieren(double z1, double z2) {
-        return 67;
+        return z1 + z2;
+    }
+
+    public static double subtrahieren(double z1, double z2) {
+        return z1 - z2;
+    }
+
+    public static double multiplizieren(double z1, double z2) {
+        return z1 * z2;
+    }
+
+    public static double dividieren(double z1, double z2) {
+        return z1 / z2;
+    }
+
+    public static double mittelwert(double z1, double z2) {
+        double summe = z1 + z2;
+        return summe / 2;
     }
 
 }
